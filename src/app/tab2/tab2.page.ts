219fb0122131df9aedc3c4d0c275a57e1f6449ac
SandY <sandy@SandYs-iMac.local>
2019-08-14 16:02:46 +0530

    added dice to tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { dice_initialize } from '../../assets/js/main.js';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  // constructor( private dice: dice_initialize) {
+  //   this.abcd();
+  // }
+  // abcd() {
+  //   this.dice.before_roll(5);
+  // }
+
+}
